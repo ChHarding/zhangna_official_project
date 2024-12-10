6a86aab8fe0600497f7a83f0ac67ce7a8be345e5
zhangna0826 <na.zhang0826@outlook.com>
2024-12-10 23:21:59 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
 # put your keys here and rename the file to keys.py
 # Important: in your .gitignore file add keys.py to avoid pushing your keys to github
-edamam_app_id =  "xxx"  
-edamam_app_key = "xxx"
+edamam_app_id =  "b9999113"  
+edamam_app_key = "9463fba28152d91257a10419ec9bf800"
